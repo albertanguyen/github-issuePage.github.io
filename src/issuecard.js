73aa73a6a169b8d4377e53943dc81f778dac09f9
daivnguyen1991 <daivnguyen1991@gmail.com>
2019-06-07 13:46:52 +0700

    done the card

--- src/issuecard.js
+++ src/issuecard.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Card, Badge } from "react-bootstrap";
+import { Container, Card, Badge, Accordion, Button } from "react-bootstrap";
 import moment from "moment";
 import MDReactComponent from "markdown-react-js";
 import "./issuecard.css";
+import { wrap } from "module";
+import { RSA_NO_PADDING } from "constants";
 
 class IssueCard extends Component {
   constructor(props) {
@@ -27,14 +29,17 @@ class IssueCard extends Component {
   renderbadges(labels) {
     return labels.map(({ color, name }) => {
       return (
-        <Badge
-          pill
-          style={{
-            backgroundColor: "#" + color
-          }}
-        >
-          {name}
-        </Badge>
+        <>
+          <Badge
+            pill
+            style={{
+              backgroundColor: "#" + color
+            }}
+          >
+            {name}
+          </Badge>
+          <br />
+        </>
       );
     });
   }
@@ -44,35 +49,56 @@ class IssueCard extends Component {
       ({ number, title, body, user, labels, state, created_at }) => {
         console.log("check body", body);
         return (
-          <Card style={{ width: "100%" }}>
-            <Card.Body>
-              <div className="d-flex">
-                <div className="mr-auto px-5 text-left">
-                  <Card.Title>
-                    <h4>
-                      #{number} {title}
-                    </h4>
-                  </Card.Title>
-                  <Card.Text>
-                    {" "}
-                    Status: {state} {moment(created_at).fromNow()}
-                  </Card.Text>
+          <Accordion>
+            <Card style={{ width: "100%" }}>
+              <Card.Body>
+                <div className="d-flex">
+                  <div className="mr-auto px-5 text-left">
+                    <Card.Title>
+                      <h4>
+                        #{number} {title}
+                      </h4>
+                    </Card.Title>
+                    <Card.Text>
+                      <br />
+                      Status: {state} // {moment(created_at).fromNow()} since it
+                      open!
+                    </Card.Text>
+                    <Accordion.Toggle as={Button} variant="link" eventKey="0">
+                      Read More!
+                    </Accordion.Toggle>
+                    <Accordion.Collapse eventKey="0">
+                      <div className="markdown">
+                        <MDReactComponent text={body} />
+                      </div>
+                    </Accordion.Collapse>
+                  </div>
                   <div>
-                    <MDReactComponent text={body} />
+                    <div className="d-block">
+                      <div className="ml-auto">
+                        <div className="text-right">
+                          <Card.Img
+                            className="rounded-circle avataimg"
+                            variant="top"
+                            src={user.avatar_url}
+                          />
+                          <p>@{user.login}</p>
+                        </div>
+                      </div>
+                      <div
+                        style={{
+                          clear: "both",
+                          textAlign: "right"
+                        }}
+                      >
+                        {this.renderbadges(labels)}
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="ml-auto">
-                  <Card.Img
-                    className="rounded-circle"
-                    variant="top"
-                    src={user.avatar_url}
-                  />
-                  <p className="text-center">{user.login}</p>
-                  <div className="text-center">{this.renderbadges(labels)}</div>
-                </div>
-              </div>
-            </Card.Body>
-          </Card>
+              </Card.Body>
+            </Card>
+          </Accordion>
         );
       }
     );
