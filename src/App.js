5afe5f69b5cf0ff8cf5ed68148bac3847d704b33
daivnguyen1991 <daivnguyen1991@gmail.com>
2019-06-07 10:29:18 +0700

    card guys, help me fix it

--- src/App.js
+++ src/App.js
@@ -1,55 +1,48 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Pagination from './pagination';
-import Search from './search';
-import IssueCard from './issuecard';
-import { logo } from './logo.svg';
-import './App.css';
-
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Pagination from "./pagination";
+import Search from "./search";
+import IssueCard from "./issuecard";
+import { logo } from "./logo.svg";
+import "./App.css";
 
 class githubIssue extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            repoName: '',
-            userName: '',
-            issueList: [],
-            isloaded: false,
-            allIssues: []
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      repoName: "",
+      userName: "",
+      issueList: [],
+      isloaded: false,
+      allIssues: []
+    };
+  }
 
-    // componentDidmount() {
+  // componentDidmount() {
 
-    // }
+  // }
 
-    render() {
-        return (
-            <div className="App">
-                {/* {navbar here} */}
-                <div className="App-header container">
-                    <img className="App-logo" src={ logo } alt="logo" />
-                    <h1 className="text-uppercase">Github issue page</h1>
-                    <div className="row">
-                        <div className="col-6">
-                            {/* {your create issue here} */}
-                            </div>                        
-                        <div className="col-12">
-                            {/* {your search field here} */}
-                        </div>
-                    </div>
-                </div>
-                <div className="App-body container">
-                    <div className="row d-flex justify-content-center">
-                    {/* {your cards here} */}
-                    </div>
-                </div>
-                <div className="App-footer">
-                </div>
-            </div>
-            )
-    }
-
+  render() {
+    return (
+      <div className="App">
+        {/* {navbar here} */}
+        <div className="App-header container">
+          <img className="App-logo" src={logo} alt="logo" />
+          <h1 className="text-uppercase">Github issue page</h1>
+          <div className="row">
+            <div className="col-6">{/* {your create issue here} */}</div>
+            <div className="col-12">{/* {your search field here} */}</div>
+          </div>
+        </div>
+        <div className="App-body container">
+          <div className="row d-flex justify-content-center">
+            <IssueCard />
+          </div>
+        </div>
+        <div className="App-footer" />
+      </div>
+    );
+  }
 }
 
 export default githubIssue;
--- src/issuecard.js
+++ src/issuecard.js
@@ -1,26 +1,90 @@
-import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './issuecard.css';
-
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Card, Badge } from "react-bootstrap";
+import moment from "moment";
+import MDReactComponent from "markdown-react-js";
+import "./issuecard.css";
 
 class IssueCard extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isloaded: false
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isloaded: false,
+      issue: []
+    };
+  }
 
-    // componentDidmount() {
+  async getData() {
+    const data = await fetch(
+      "https://api.github.com/repos/facebook/react/issues"
+    );
+    const myData = await data.json();
+    console.log("data", myData);
+    this.setState({ issue: myData });
+    console.log("check my datat in console", this.state.issue);
+  }
 
-    // }
+  renderbadges(labels) {
+    return labels.map(({ color, name }) => {
+      return (
+        <Badge
+          pill
+          style={{
+            backgroundColor: "#" + color
+          }}
+        >
+          {name}
+        </Badge>
+      );
+    });
+  }
 
-    render() {
+  renderCard() {
+    return this.state.issue.map(
+      ({ number, title, body, user, labels, state, created_at }) => {
+        console.log("check body", body);
         return (
-            <p>issue card</p>            
-        )
-    }
+          <Card style={{ width: "100%" }}>
+            <Card.Body>
+              <div className="d-flex">
+                <div className="mr-auto px-5 text-left">
+                  <Card.Title>
+                    <h4>
+                      #{number} {title}
+                    </h4>
+                  </Card.Title>
+                  <Card.Text>
+                    {" "}
+                    Status: {state} {moment(created_at).fromNow()}
+                  </Card.Text>
+                  <div>
+                    <MDReactComponent text={body} />
+                  </div>
+                </div>
+                <div className="ml-auto">
+                  <Card.Img
+                    className="rounded-circle"
+                    variant="top"
+                    src={user.avatar_url}
+                  />
+                  <p className="text-center">{user.login}</p>
+                  <div className="text-center">{this.renderbadges(labels)}</div>
+                </div>
+              </div>
+            </Card.Body>
+          </Card>
+        );
+      }
+    );
+  }
 
+  componentDidMount() {
+    this.getData();
+  }
+
+  render() {
+    return <Container>{this.renderCard()}</Container>;
+  }
 }
 
 export default IssueCard;
