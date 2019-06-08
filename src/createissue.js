839e2f407da8eb22301af576ea59642a8bda0f12
Hoa <huynhtehoa@gmail.com>
2019-06-08 20:13:28 +0700

    hoa-test-with-create-issue

--- src/createissue.js
+++ src/createissue.js
@@ -5,7 +5,8 @@ import {
     Form,
     FormControl,
     Nav,
-    Modal
+    Modal,
+    Card, Badge, Accordion
 } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -17,6 +18,7 @@ import './createissue.css';
 
 
 class MyVerticallyCenteredModal extends React.Component {
+
     render() {
         return (
             <Modal
@@ -29,23 +31,32 @@ class MyVerticallyCenteredModal extends React.Component {
                     <Navbar variant="dark">
                         <Nav className="mr-auto">
                         </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Issue subject" className="mr-sm-2" />
+                        <Form>
+                            <FormControl type="text" placeholder="Issue subject" className="mr-sm-2"
+                                onChange={e => this.props.onChangeOne(e)}
+                            />
                         </Form>
                     </Navbar>
                 </Modal.Header>
                 <Modal.Body>
                     <Form.Group controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Describe your issue below</Form.Label>
-                        <Form.Control as="textarea" rows="10" />
+                        <Form.Control as="textarea" rows="10"
+                            onChange={e => this.props.onChangeTwo(e)}
+                        />
                     </Form.Group>
                 </Modal.Body>
                 <Modal.Footer style={{ backgroundColor: "#282c34" }}>
                     <Navbar variant="dark">
                         <Button
                             className="mr-auto"
-                            onClick={this.props.onHide}
                             variant="outline-info"
+                            onClick={
+                                () => {
+                                    this.props.onHide();
+                                    this.props.submitLocal();
+                                }
+                            }
                         >
                             Submit issue
                             </Button>
@@ -62,27 +73,92 @@ class Createissue extends Component {
         super(props)
         this.state = {
             isloaded: true,
-            modalShow: false
+            modalShow: false,
+            titleLocal: "",
+            bodyLocal: "",
         };
     }
+
+    titleLocal = e => {
+        this.setState({ titleLocal: e.target.value }, () => console.log("title", this.state.titleLocal));
+    };
+
+    bodyLocal = e => {
+        this.setState({ bodyLocal: e.target.value }, () => console.log("body", this.state.bodyLocal));
+    };
+
+    submitLocal = () => {
+        console.log("local", this.state.titleLocal)
+        this.renderSubmit()
+        this.setState({
+            titleLocal: this.state.titleLocal,
+            bodyLocal: this.state.bodyLocal
+        }
+        )
+    }
+
+    renderSubmit = function () {
+        console.log("rendersubmit", this.state.titleLocal)
+
+        return (
+            <Accordion>
+                <Card>
+                    <Card.Body>
+                        <div className="d-flex">
+                            <div className="mr-auto text-left">
+                                <Card.Title>
+                                    <h4 style={{ fontSize: 20 }}>
+                                        #{this.state.titleLocal}
+                                        &nbsp;
+                                        </h4>
+
+                                </Card.Title>
+                                <Card.Text>
+                                    <Accordion.Toggle as={Button} variant="link" style={{ paddingLeft: 0 }}>
+                                        Show Body Text
+                    </Accordion.Toggle>
+                                    <Accordion.Collapse>
+                                        {this.state.bodyLocal}
+                                    </Accordion.Collapse>
+                                </Card.Text>
+
+                            </div>
+
+                        </div>
+                    </Card.Body>
+                </Card>
+            </Accordion>
+        )
+
+    }
+
     render() {
         let modalClose = () => this.setState({ modalShow: false });
 
         return (
-            <Navbar expand="lg" variant="dark">
-                <div className="d-flex">
-                    <Button className="issue-btn"
-                        variant="outline-info"
-                        style={{ width: '150px', fontSize: "0.7em" }}
-                        onClick={() => this.setState({ modalShow: true })}
-                    >Create issue</Button>
-                    <img className="App-logo" src={'img/logo.svg'} alt="logo" />
-                    <MyVerticallyCenteredModal
-                        show={this.state.modalShow}
-                        onHide={modalClose}
-                    />
+            <>
+                <Navbar expand="lg" variant="dark">
+                    <div className="d-flex">
+                        <Button className="issue-btn"
+                            variant="outline-info"
+                            style={{ width: '150px', fontSize: "0.7em" }}
+                            onClick={() => this.setState({ modalShow: true })}
+                        >Create issue</Button>
+                        <img className="App-logo" src={'img/logo.svg'} alt="logo" />
+                        <MyVerticallyCenteredModal
+                            show={this.state.modalShow}
+                            onHide={modalClose}
+                            onChangeOne={e => this.titleLocal(e)}
+                            onChangeTwo={e => this.bodyLocal(e)}
+                            submitLocal={this.submitLocal}
+                        />
+                    </div>
+                </Navbar>
+                <div style={{ backgroundColor: "white", color: "red", width: 200, height: 200 }}>
+                    asdsad
+        {this.renderSubmit()}
                 </div>
-            </Navbar>
+            </>
         );
     }
 }
